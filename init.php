75ce4726d2a1bdb40259a6692495722ce60a5593
GMG262000 <143028145+GMG262000@users.noreply.github.com>
2024-08-30 16:16:47 -0300

    uptean

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+
+//definicao do timezone
+
+date_default_timezone_get ('America/Sao Paulo');
+
+//conxeao para banco de dados
+
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_SENHA','');
+define('BD_BANCO','livraria');
+?>
